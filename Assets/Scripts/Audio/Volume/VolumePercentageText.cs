32e487ccf93087629a389145905ea3f0f3481b4f
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-09-19 00:52:57 -0600

    PauseMenuReferences

--- Assets/Scripts/Audio/Volume/VolumePercentageText.cs
+++ Assets/Scripts/Audio/Volume/VolumePercentageText.cs
@@ -1,16 +1,80 @@
+using TMPro;
 using UnityEngine;
 
 public class VolumePercentageText : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private enum Channel
     {
-        
+        Master,
+        Background,
+        SFX, 
+        UI
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Data")]
+    [SerializeField] private AudioDataSo data;
+
+    [Header("Config")]
+    [SerializeField] private Channel channel;
+
+    [Header("Visuals")]
+    [SerializeField] private TMP_Text percentageText;
+
+    private void OnEnable()
     {
-        
+        UpdateText(GetCurrentValue());
+
+        switch (channel)
+        {
+            case Channel.Master:
+                data.OnMasterVolumeChanged += UpdateText;
+                break;
+            case Channel.Background:
+                data.OnBackgroundVolumeChanged += UpdateText;
+                break;
+            case Channel.SFX:
+                data.OnSfxVolumeChanged += UpdateText;
+                break;
+            case Channel.UI:
+                data.OnUiVolumeChanged += UpdateText;
+                break;
+        }
+    }
+
+    private void OnDisable()
+    {
+        switch (channel)
+        {
+            case Channel.Master:
+                data.OnMasterVolumeChanged -= UpdateText;
+                break;
+            case Channel.Background:
+                data.OnBackgroundVolumeChanged -= UpdateText;
+                break;
+            case Channel.SFX:
+                data.OnSfxVolumeChanged -= UpdateText;
+                break;
+            case Channel.UI:
+                data.OnUiVolumeChanged -= UpdateText;
+                break;
+        }
+    }
+
+    private void UpdateText(float value)
+    {
+        int percentage = Mathf.RoundToInt(value * 100f);
+        percentageText.text = percentage + "%";
+    }
+
+    private float GetCurrentValue()
+    {
+        switch (channel)
+        {
+            case Channel.Master: return data.MasterVolume;
+            case Channel.Background: return data.BackgroundVolume;
+            case Channel.SFX: return data.SfxVolume;
+            case Channel.UI: return data.UiVolume;
+            default: return 1f;
+        }
     }
 }
